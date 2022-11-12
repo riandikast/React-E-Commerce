98e06995f0f3e51c7661c9d69090119c624edcf6
Riandika <riandikasp@gmail.com>
2022-11-12 17:43:48 +0700

    Remove search icon from admin role

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../store/login/LoginSlice";
 import Swal from "sweetalert2";
 function Navbar() {
-  const [refresh, setRefresh] = useState('')
+  const [refresh, setRefresh] = useState("");
   const location = useLocation();
   const dispatch = useDispatch();
   const [path, setPath] = useState(null);
@@ -19,32 +19,30 @@ function Navbar() {
   }, [location]);
 
   useEffect(() => {
-    setRefresh('')
+    setRefresh("");
   }, [refresh]);
 
   const handleLogout = async () => {
     Swal.fire({
-      title: 'Are you sure want to logout?',
+      title: "Are you sure want to logout?",
       text: "",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes',
-      reverseButtons : true
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+      reverseButtons: true,
     }).then((result) => {
       if (result.isConfirmed) {
         dispatch(logoutUser());
         setIsAdmin(false);
-        setRefresh('Refresh')
-  
+        setRefresh("Refresh");
       }
-    })
-   
+    });
   };
 
   useEffect(() => {
-    if (adminCheck !==null){
+    if (adminCheck !== null) {
       for (let i = 0; i < adminCheck.length; i++) {
         if (adminCheck[i].admin === true) {
           return setIsAdmin(true);
@@ -53,7 +51,6 @@ function Navbar() {
         }
       }
     }
-
   }, [adminCheck || isAdmin]);
 
   return (
@@ -119,44 +116,47 @@ function Navbar() {
             >
               Rekap Penjualan
             </NavLink>
-    
           </div>
         )}
 
         <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 mr-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
-          {isLogin && (
+          {!isAdmin && (
             <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 mr-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-            />
-          </svg>
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          )}
+
+          {isLogin && (
+            <Link to="/cart">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 mr-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </Link>
           )}
           {isLogin || isAdmin ? (
             <>
-    
               <button
                 className="bg-[#cf6137] py-1 px-4 text-white font-base rounded-md ml-3"
                 onClick={handleLogout}
