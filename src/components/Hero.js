46ff8d7b24ef0c7ec1a4b08737b5f0bec83c1dbc
Riandika <riandikasp@gmail.com>
2022-11-17 00:59:15 +0700

    Improve banner on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+import {scroller } from "react-scroll";
 function Hero() {
     return (
         <div className="relative flex justify-around items-center w-screen h-[90vh] !bg-dark-plain">
@@ -5,7 +6,7 @@ function Hero() {
                 <h1 className="text-4xl text-darkgreen font-bold mb-2">Exclusive Deals of </h1>
                 <h1 className="text-4xl text-darkgreen font-bold mb-5">Women's Clothing</h1>
                 <p className="text-darkgreen">Explore different categories. Find the best deals.</p>
-                <button className="bg-green px-5 py-2 mt-5 rounded-md text-white font-semibold">Shop Now</button>
+                <button onClick={()=>scroller.scrollTo('productElement',{smooth:true, spy:true, duration:700})} className="bg-green px-5 py-2 mt-5 rounded-md text-white font-semibold">Shop Now</button>
             </div>
             <div className="flex">
                 <img src="/hero.svg" alt="Hero" width={500}  />
